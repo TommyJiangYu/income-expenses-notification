e66fb247a07721e9c01c05a414909df3e1487e78
TommyJ <pigpormwork@gmail.com>
2024-12-16 19:25:20 +0700

    [GG-01] - Connect line success with test get event and reply message.

--- backend/src/line-messaging/line-messaging.controller.ts
+++ backend/src/line-messaging/line-messaging.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { LineMessagingService } from './line-messaging.service';
+import { WebhookRequestBody } from '@line/bot-sdk';
 
 @Controller('/line-messaging')
 export class LineMessagingController {
   constructor(private lineMessagingService: LineMessagingService) {}
-  @Get('/webhook')
-  getWebhookResponse() {
-    return this.lineMessagingService.getMessageFromLine();
+
+  @Get('/test-connection')
+  testConnection() {
+    return {
+      status: 200,
+      message: 'test connection successful',
+    };
+  }
+
+  @Post('/webhook')
+  getWebhookResponse(@Body() body: WebhookRequestBody) {
+    try {
+      return this.lineMessagingService.getEventFromLine(body.events);
+    } catch (err) {
+      throw new BadRequestException(
+        'Cannot get event from line because : ',
+        err,
+      );
+    }
   }
 }
--- backend/src/line-messaging/line-messaging.service.ts
+++ backend/src/line-messaging/line-messaging.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { messagingApi } from '@line/bot-sdk';
+import { ConfigService } from '@nestjs/config';
+import {
+  Message,
+  MessageEvent,
+  messagingApi,
+  WebhookEvent,
+} from '@line/bot-sdk';
+const { MessagingApiClient } = messagingApi;
 
 @Injectable()
 export class LineMessagingService {
-  getMessageFromLine() {
-    return { message: 'from line' };
+  configService: ConfigService;
+  private client: messagingApi.MessagingApiClient;
+
+  constructor(configService: ConfigService) {
+    this.configService = configService;
+    this.client = new MessagingApiClient({
+      channelAccessToken: configService.get('LINE_CHANNEL_ACCESS_TOKEN'),
+    });
+  }
+
+  async getEventFromLine(events: WebhookEvent[]) {
+    events.map((eventItem: MessageEvent) => {
+      if (eventItem.type !== 'message') {
+        return null;
+      }
+      this.replyMessage(eventItem);
+    });
+
+    return { success: true };
+  }
+
+  replyMessage(event: MessageEvent) {
+    const message: Message = {
+      type: 'text',
+      text: 'เกียกำลังเขียน Nest อยู่ ไม่ว่าง...',
+    };
+
+    this.client.replyMessage({
+      replyToken: event.replyToken,
+      messages: [message],
+    });
   }
 }
